d23fcd0d9a65e387477de137e1096d9a56e9d411
NoHeal4u <miodragnikolic@yahoo.com>
2018-05-18 20:46:44 +0200

    treci uradjen

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,17 +8,9 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <router-link to="/chuck">Chuck</router-link>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
+      
     </ul>
   </div>
 </nav>
