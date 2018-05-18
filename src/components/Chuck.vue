dd1a38dc69d6d73d9f0fbde7d7dc9b739dbebb6b
NoHeal4u <miodragnikolic@yahoo.com>
2018-05-18 21:21:45 +0200

    cetvrti uradjen

--- src/components/Chuck.vue
+++ src/components/Chuck.vue
@@ -1,8 +1,7 @@
 <template>
 	<div class="container">
 
-	<template>
-    <div class="container"> 
+	
   <!--  Dje si Chuck! {{ randomJoke.value }}  -->
    <div class="card" style="width: 18rem;">
   <img class="card-img-top" 
@@ -12,6 +11,12 @@
      <p class="card-text"
      v-text="randomJoke.value">
      </p>
+     <input class="form-control mb-2"
+      type="text"
+       placeholder= "Enter joke category..."
+        @input="setCategory"
+        >
+     <a @click="getNewJoke" class="btn btn-primary">Get new joke</a>
   </div>
 </div>
     </div>
@@ -19,12 +24,11 @@
 		
 
 
-	</div>
-</template>
+	
 
 <script>
 
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions, mapMutations } from 'vuex'; //map actions vise nije potreban jer smo import store
 import { store } from './../store';
 
 //[1,2,3]=> ...[1,2,3] => 1,2,3
@@ -39,9 +43,27 @@ import { store } from './../store';
 		},
 
 		methods: {
-			...mapActions([
-				'fetchRandomJoke'])
+
+			...mapMutations([
+
+				'setJokeCategory'
+
+			]),
+
+			getNewJoke(){
+				store.dispatch('fetchRandomJoke', ()=>{} ); //anonimna da bi mogla next bez pucanja
+			},
+
+			setCategory(event){
+				this.setJokeCategory(event.target.value);
+
+			},
 		},
+
+		// methods: {
+		// 	...mapActions([
+		// 		'fetchRandomJoke'])
+		// }, ne treba vise jer smo isli preko stora
 
 		// created(){
 		// 	this.fetchRandomJoke();
